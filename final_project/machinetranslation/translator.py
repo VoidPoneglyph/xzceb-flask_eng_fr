17bb67f061b7925bb3aacd0c8d03300a7bba8f47
VoidPoneglyph <themednobility@gmail.com>
2021-09-17 13:13:35 +0000

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,27 @@
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2018-05-01'
+, authenticator=authenticator)
+language_translator.set_service_url(url)
+
+def english_to_french(english_text): #This function converts English Text stored in the variable
+    #english_text to french.
+    french_text = language_translator.translate(text = english_text,
+    model_id = 'en-fr').get_result()
+    french_text = french_text['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text): #This function converts french text stored in the variable
+    #french_Text to english
+    english_text = language_translator.translate(text = french_text,
+     model_id = 'fr-en').get_result()
+    english_text = english_text['translations'][0]['translation']
+    return english_text
