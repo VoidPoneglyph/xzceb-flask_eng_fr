46e2a273f451716af83eea2223ef6595e66ed364
VoidPoneglyph <89205452+VoidPoneglyph@users.noreply.github.com>
2021-09-17 23:04:55 +0530

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -3,8 +3,11 @@ from translator import english_to_french, french_to_english
 class TestMyModule(unittest.TestCase):
     def test_e2f(self):
         self.assertEqual(english_to_french('Hello'), "Bonjour")
+        self.assertNotEqual(english_to_french('Hello'), "Hello")
     def test_f2e(self):
         self.assertEqual(french_to_english('Bonjour'), "Hello")
+        self.assertNotEqual(french_to_english('Bonjour'), "Bonjour")
+
 
 if __name__ == '__main__':
     unittest.main()
